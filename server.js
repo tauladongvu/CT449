fc46d3c27734e169ac29730519faa24a04b5730e
Dong Vu <vub1906614@student.ctu.edu.vn>
2022-03-05 13:52:46 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const { default: mongoose } = require("mongoose");
 const app = require("./app");
 const config = require("./app/config");
 
+//connect to database
+mongoose.connect(config.db.uri)
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((error) => {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    });
+    
 //start server
 const PORT = config.app.port;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
